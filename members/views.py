30cf9705162ab8a8af50c634aac81a75e25f2b7c
Pankaj Padekar <padekarpankaj@gmail.com>
2024-02-08 13:39:50 +0530

    added details

--- members/views.py
+++ members/views.py
@@ -8,7 +8,17 @@ def members(request):
     mymembers = Members.objects.all().values()
     template = loader.get_template("all_members.html")
     context = {
-        'mymembers' : mymembers
+        'mymembers' : mymembers,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def details(request , id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {
+        'mymember' : mymember,
     }
 
     return HttpResponse(template.render(context, request))

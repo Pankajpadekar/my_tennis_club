d19a822fc7e2307a10050f0211c9cb7ee5f27d21
Pankaj Padekar <padekarpankaj@gmail.com>
2024-02-08 12:48:00 +0530

    added template files

--- members/views.py
+++ members/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 # Create your views here.
 
 def members(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {
+        'mymembers' : mymembers
+    }
 
-    template = loader.get_template("myfirst.html")
-
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
 
